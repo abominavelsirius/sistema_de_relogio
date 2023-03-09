250e20050f93bae2377082bb54882be972911814
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-09 19:16:04 -0300

    finalizando por hoje

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert('Olá!');
+
+
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date()
+    var hora = data.getHours()
+//  var hora = 22
+
+    msg.innerHMTL = `Agora são ${hora} horas.`
+
+    if(hora >= 6 && hora <= 12) {
+        // BOM DIA!
+        img.src = './img/manha.png'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = './img/tarde.png'
+    } else {
+        // BOA NOITE!
+        img.src = './img/noite.png'
+    }
+
+}
